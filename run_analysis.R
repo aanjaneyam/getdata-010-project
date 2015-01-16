df5ab7d991fdd710d058a8e00ff8428918bec0a9
Aanjaneya Mishra <tomtum2004-linux@yahoo.com>
2015-01-16 17:56:26 +0000

    updated run_analysis.R to add code for naming variables and merging data sets

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
-#### Download the project file into folder named project_data
+##################################################################
+# Download the project file into folder named project_data
+##################################################################
 
 # if folder project_data does not exist creat it.
 if(!file.exists("./project_data")){
@@ -12,13 +14,16 @@ download.file(fileUrl, destfile, method = "curl")
 dateDownloaded <- date()
 dateDownloaded
 
-#### Unzip the file downloaded file and place all its content in UCI HAR Dataset directory
+##################################################################
+# Unzip the file downloaded file and place all its content in UCI HAR Dataset directory
+##################################################################
 unzip(destfile, exdir="./project_data")
 
 path_rf <- "./project_data/UCI HAR Dataset"
 files<-list.files(path_rf, recursive=TRUE)
 files
 
+##################################################################
 #### To complete the project we just need the the following files from the data (files in the "Inertial Signals" directory are not needed)
 
     #UCI HAR Dataset/test/subject_test.txt
@@ -29,8 +34,11 @@ files
     #UCI HAR Dataset/train/y_train.txt
 	#UCI HAR Dataset/features.txt
 	#UCI HAR Dataset/activity_labels.txt
+##################################################################
 
-#### Load data
+##################################################################
+# Load data
+##################################################################
 
 # read test dataset's activity, features and subjects
 test_data_activity <- read.table("./project_data/UCI HAR Dataset/test/y_test.txt")
@@ -42,6 +50,24 @@ train_data_activity <- read.table("./project_data/UCI HAR Dataset/train/y_train.
 train_data_features <- read.table("./project_data/UCI HAR Dataset/train/X_train.txt")
 train_data_subject <- read.table("./project_data/UCI HAR Dataset/train/subject_train.txt")
 
-# read activity lables 
+# read activity lables and feature names
 activity_labels <- read.table("./project_data/UCI HAR Dataset/activity_labels.txt")
 feature_names <- read.table("./project_data/UCI HAR Dataset/features.txt")
+
+##################################################################
+# Merges the training and the test sets to create one data set.
+##################################################################
+
+# set names to variables 
+names(test_data_activity) <- c("activity")
+names(test_data_features) <- feature_names$V2
+names(test_data_subject) <- c("subject")
+names(train_data_activity) <- c("activity")
+names(train_data_features) <- feature_names$V2
+names(train_data_subject) <- c("subject")
+
+# Merge the data to create one data set
+
+test_data <- cbind(cbind(test_data_features, test_data_subject), test_data_activity)
+train_data <- cbind(cbind(train_data_features, train_data_subject), train_data_activity)
+big_data <- rbind(test_data, train_data)
